2faef288492bf9190cab459400e89ec14849d3b5
Toby <tobydeh@gmail.com>
2026-02-24 21:23:14 +0000

    refactor(widgets): remove redundant EnsureLayer override

--- pkg/widgets/view.go
+++ pkg/widgets/view.go
@@ -45,10 +45,6 @@ func (r *renderView) IsRepaintBoundary() bool {
 	return true
 }
 
-func (r *renderView) EnsureLayer() *graphics.Layer {
-	return r.RenderBoxBase.EnsureLayer()
-}
-
 func (r *renderView) SetChild(child layout.RenderObject) {
 	setParentOnChild(r.child, nil)
 	r.child = setChildFromRenderObject(child)
